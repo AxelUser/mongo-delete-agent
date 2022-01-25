16cfe1a2db91664267bbb4b735fbf375e07cc960
Alexey Maltsev <axel.user.perm@gmail.com>
2022-01-25 22:59:53 +0300

    Renamed sharded cluster composition and added new lines for init script

--- docker-compose/init-cluster.sh
+++ docker-compose/init-cluster.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-echo "Init cluster step 1: Start all of the containers"
+printf "Init cluster step 1: Start all of the containers\n"
 docker-compose up -d --force-recreate
 
-echo "Waiting 20 seconds for all containers to start"
+printf "\nWaiting 20 seconds for all containers to start\n"
 sleep 20s
 
-echo "Init cluster step 2.1: Initialize config servers rs"
+printf "\nInit cluster step 2.1: Initialize config servers rs\n"
 docker-compose exec -T configsvr01 sh -c "mongo < /scripts/init-configserver.js"
 
-echo "Init cluster step 2.2: Initialize shard 1 rs"
+printf "\nInit cluster step 2.2: Initialize shard 1 rs\n"
 docker-compose exec -T shard01-a sh -c "mongo < /scripts/init-shard01.js"
 
-echo "Init cluster step 2.3: Initialize shard 2 rs"
+printf "\nInit cluster step 2.3: Initialize shard 2 rs\n"
 docker-compose exec -T shard02-a sh -c "mongo < /scripts/init-shard02.js"
 
-echo "Init cluster step 2.4: Initialize shard 3 rs"
+printf "\nInit cluster step 2.4: Initialize shard 3 rs\n"
 docker-compose exec -T shard03-a sh -c "mongo < /scripts/init-shard03.js"
 
-echo "Waiting 50 seconds for all replica sets to elect their primaries"
+printf "\nWaiting 50 seconds for all replica sets to elect their primaries\n"
 sleep 50s
 
-echo "Init cluster step 3: Initializing the router"
+printf "\nInit cluster step 3: Initializing the router\n"
 docker-compose exec -T router01 sh -c "mongo < /scripts/init-router.js"
+printf "\n"
